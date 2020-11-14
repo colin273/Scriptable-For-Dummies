090186719794ae3716a79b6dd70bb566ac52c25d
IssamElNass <issamelnasri@gmail.com>
2020-11-14 03:38:50 +0100

    Team Update

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export default function TeamCard({
   className,
   name,
   children,
+  imageUrl,
   githubUrl,
   twitterUrl,
 }) {
@@ -12,16 +13,18 @@ export default function TeamCard({
       <div className="card card--full-height">
         <div className="card__header">
           <div className="avatar avatar--vertical">
-            <img
-              className="avatar__photo avatar__photo--xl"
-              src={githubUrl + '.png'}
-            />
+            {imageUrl && (
+              <img
+                className="avatar__photo avatar__photo--xl"
+                src={imageUrl + ".png"}
+              />
+            )}
             <div className="avatar__intro">
               <h3 className="avatar__name">{name}</h3>
             </div>
           </div>
         </div>
-        <div className="card__body">{children}</div>
+        <div className="card__body" style={{textAlign: 'center'}}>{children}</div>
         <div className="card__footer">
           <div className="button-group button-group--block">
             {githubUrl && (
