aab16945afd423c4416f84afe535a49c1630ba9b
FifiTheBulldog <63314105+FifiTheBulldog@users.noreply.github.com>
2021-04-04 14:11:47 -0400

    Format sidebars.js
    Each item now has its own line for better readability, so it's easier to maintain this file

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,20 @@
 module.exports = {
   docs: {
-    Welcome: ['about', 'what-is-scriptable'],
-    "📌 Introduction": ['getting-started', 'learning-resources', 'simple-alert'],
-    "⚙️ Guides": ['add-text-widget', 'add-image-widget'],
+    Welcome: [
+      'about',
+      'what-is-scriptable'
+    ],
+    "📌 Introduction": [
+      'getting-started',
+      'learning-resources',
+      'simple-alert'
+    ],
+    "⚙️ Guides": [
+      'add-text-widget',
+      'add-image-widget'
+    ]
   },
   api: [
     'api/alert',
-  ],
+  ]
 };
